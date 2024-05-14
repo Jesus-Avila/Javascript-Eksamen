243fafa55abbfeba375c3c7c16153d22fcd190e1
Jesus Avila <jesus.shark27@hotmail.com>
2024-05-14 16:02:56 +0200

    load more cocktails by category

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
 import { cocktailCard } from "./card.js";
-import { fetchTenRandomCocktails } from "./api.js";
+import { fetchTenRandomCocktails, fetchRandomCocktail } from "./api.js";
 
 let cocktailsArray = [];
-
+let selectedFilter = null;
 
 // Fetch 10 more random cocktails
 // Append the new cocktails cocktail-list in the DOM
 const fetchMoreCocktails = async () => {
   let newArray = [];
-  fetchTenRandomCocktails().then((cocktails) => {
-    newArray = cocktails;
-    const existingCocktails = JSON.parse(sessionStorage.getItem("cocktails")) || [];
-    const updateCocktails = existingCocktails.concat(newArray);
-    // Save array in sessionStorage in addition to the current array
-    sessionStorage.setItem("cocktails", JSON.stringify(updateCocktails));
-    newArray.forEach((cocktail) => {
-      cocktailCard(cocktail);
-    });
+  console.log("Selected filter", selectedFilter);
+  if (!selectedFilter) {
+    newArray = await fetchTenRandomCocktails();
+  } else {
+    const category = selectedFilter;
+    newArray = await fetchTenCocktailsByCategory(category);
+  }
+
+  const existingCocktails = JSON.parse(sessionStorage.getItem("cocktails")) || [];
+  const updateCocktails = existingCocktails.concat(newArray);
+  // Save array in sessionStorage in addition to the current array
+  sessionStorage.setItem("cocktails", JSON.stringify(updateCocktails));
+  newArray.forEach((cocktail) => {
+    cocktailCard(cocktail);
   });
+  //   });
+};
+
+// Fetch 10 random cocktails and return them until the array has 10 cocktails in it and they are of specific category
+const fetchTenCocktailsByCategory = async (category) => {
+  const cocktails = [];
+  while (cocktails.length < 10) {
+    const cocktail = await fetchRandomCocktail();
+    if (cocktail.strCategory === category) {
+      // Save cocktail in cocktails array
+      cocktails.push(cocktail);
+    }
+    if (cocktails.length >= 10 && cocktails.every((cocktail) => cocktail.strCategory !== category)) {
+      break;
+    }
+  }
+  return cocktails;
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -32,33 +54,50 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 });
 
+// All filter button
+const allButton = document.querySelector("#all-cocktails-filter");
+allButton.addEventListener("click", async () => {
+  console.log("All cocktails button clicked");
+  const array = JSON.parse(sessionStorage.getItem("cocktails"));
+  console.log("All cocktails array", array);
+  updateCocktailList(array);
+});
+
 // Filter buttons
 const filterButtons = document.querySelectorAll(".filter-button");
 filterButtons.forEach((button) => {
-    button.addEventListener("click", async () => {
+  button.addEventListener("click", async () => {
+    const filter = button.textContent;
+    console.log("Filter button clicked", filter);
 
-        const filter = button.textContent;
-        let filteredCocktails = [];
-        console.log("Filter button clicked", filter);
-        let array = JSON.parse(sessionStorage.getItem("cocktails"));
-        // Filter cocktails from sessionStorage and update the cocktail list
-        filteredCocktails = array.filter((cocktail) => cocktail.strCategory === filter);
-        console.log('array of arrays', filteredCocktails);
-        // filteredCocktails = await fetchTenCocktailsByCategory(filter);
-        updateCocktailList(filteredCocktails);
-    });
-})
+    selectedFilter = filter;
+    filterButtons.forEach((btn) => btn.classList.remove("selected"));
+    button.classList.add("selected");
+    console.log("Selected filter", selectedFilter);
+
+    let filteredCocktails = [];
+    let array = JSON.parse(sessionStorage.getItem("cocktails"));
+
+    if (selectedFilter === "All") {
+      updateCocktailList(array);
+    } else {
+      filteredCocktails = array.filter((cocktail) => cocktail.strCategory === filter);
+      console.log("array of arrays", filteredCocktails);
+      updateCocktailList(filteredCocktails);
+    }
+  });
+});
 
 // Update cocktail list in the DOM
 const updateCocktailList = (cocktails) => {
-    const cocktailList = document.querySelector(".cocktail-list");
-    // Clear current cocktail list
-    cocktailList.innerHTML = "";
-    // Append new cocktails
-    cocktails.forEach((cocktail) => {
-      cocktailCard(cocktail);
-    });
-  };
+  const cocktailList = document.querySelector(".cocktail-list");
+  // Clear current cocktail list
+  cocktailList.innerHTML = "";
+  // Append new cocktails
+  cocktails.forEach((cocktail) => {
+    cocktailCard(cocktail);
+  });
+};
 
 // Load more button
 const loadMoreButton = document.querySelector("#load-more");
